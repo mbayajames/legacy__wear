6bb11cceb7271382919253aa8068acb150689d1f
mbayajames <jmuthiora244@gmail.com>
2026-02-10 05:01:05 -0800

    Update Clothes page component

---
+++ src/pages/Clothes.tsx
@@ -0,0 +1,14 @@
+import { ShopPage } from './ShopPage';
+
+const Clothes = () => {
+  return (
+    <ShopPage
+      category="clothes"
+      title="Clothes"
+      description="Explore our stylish clothing collection"
+      bannerImage="https://images.unsplash.com/photo-1441986300917-64674bd600d8?w=1200"
+    />
+  );
+};
+
+export default Clothes;
