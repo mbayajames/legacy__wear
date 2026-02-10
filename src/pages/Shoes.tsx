1263b26d02e2b32d9f50a3d1abd8c881b3b33ec7
mbayajames <jmuthiora244@gmail.com>
2026-02-10 04:56:00 -0800

    Update Shoes page component

---
+++ src/pages/Shoes.tsx
@@ -0,0 +1,14 @@
+import { ShopPage } from './ShopPage';
+
+const Shoes = () => {
+  return (
+    <ShopPage
+      category="shoes"
+      title="Shoes"
+      description="Step into style with our premium footwear collection"
+      bannerImage="https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=1920&q=80"
+    />
+  );
+};
+
+export default Shoes;
