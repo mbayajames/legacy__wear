8926554353843bd206b452ba435dc290be202b92
mbayajames <jmuthiora244@gmail.com>
2026-02-10 05:05:36 -0800

    Update useMobile hook

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,28 +1,19 @@
-import { useState, useEffect } from "react";
+import * as React from "react";
+
+const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined);
 
-  useEffect(() => {
-    const checkIsMobile = () => {
-      setIsMobile(window.innerWidth < 768);
+  React.useEffect(() => {
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+    const onChange = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
     };
-
-    // Initial check
-    checkIsMobile();
-
-    // Add event listener for window resize
-    const handleResize = () => {
-      checkIsMobile();
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    // Cleanup
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+    mql.addEventListener("change", onChange);
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    return () => mql.removeEventListener("change", onChange);
   }, []);
 
-  return isMobile;
+  return !!isMobile;
 }
