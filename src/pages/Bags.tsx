5fc460a2f1e238eb97fb8d8612f92640db070764
mbayajames <jmuthiora244@gmail.com>
2026-02-10 05:02:44 -0800

    Update Bags page component

---
+++ src/pages/Bags.tsx
@@ -0,0 +1,14 @@
+import { ShopPage } from './ShopPage';
+
+const Bags = () => {
+  return (
+    <ShopPage
+      category="bags"
+      title="Bags"
+      description="Discover our collection of premium bags and accessories"
+      bannerImage="https://images.unsplash.com/photo-1548036328-c9fa89d128fa?w=1920&q=80"
+    />
+  );
+};
+
+export default Bags;
